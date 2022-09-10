03c71445d61b509e027415137f1df96b23e71dd4
Roberto <rober.autom@gmail.com>
2022-09-10 10:08:36 -0300

    Initial commit

---
+++ src/main/kotlin/AppSegundoPrograma.kt
@@ -0,0 +1,14 @@
+fun main(){
+    //print
+    //println, /n
+    //var e val
+    //\t tabular
+
+    var variavelA = "Programação"
+    var variavelB = "Roberto Santos"
+
+    print("Programação\nKotlin")
+    println("\nProgramação\t\tKotlin")
+    println("$variavelA\t\t$variavelB")
+
+}
